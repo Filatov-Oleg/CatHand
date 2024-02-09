0ac767249bfcb43c81f60c730ad9ce7e47fd2082
Filatov <o.filatov@rendez-vous.ru>
2024-02-09 18:35:37 +0300

    Start project

---
+++ CatHand/CatHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatHandApp.swift
+//  CatHand
+//
+//  Created by Филатов Олег Олегович on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CatHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
